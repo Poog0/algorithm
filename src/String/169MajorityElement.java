da38126bd59b6ebb55ed512bacae891610792d4a
邓平光 <76396330+Poog0@users.noreply.github.com>
2020-12-20 22:25:50 +0800

    Update 169MajorityElement.java

--- src/String/169MajorityElement.java
+++ src/String/169MajorityElement.java
@@ -1,3 +1,4 @@
+//第一种写法
 public static int majorityElement(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
         int[] nums2;
@@ -15,3 +16,32 @@ public static int majorityElement(int[] nums) {
         }
         return -1;
     }
+//第二种写法(摩尔算法)
+//时间复杂度O(n)
+//空间复杂度O(1)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int temp = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == temp) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                temp = nums[i];
+                count = 1;
+            }
+        }
+
+        // 验证是否满足要求
+        int t = nums.length / 2 + 1;
+        count = 0;
+        for (int num : nums) {
+            if (num == temp) count++;
+            if (count == t) return temp;
+        }
+        return -1;
+    }
+}
